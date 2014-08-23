62cfdb0a8e4c3e63f9ab3b6ab79c381241c8029f
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2014-08-23 15:13:53 +1200

    player movable + now in an entity class + list

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ WIDTH = 320
 HEIGHT = 200
 SCALE = 3
 
+FPS = 60.0
+SPF = 1.0/FPS
+
 # Set up display
 pygame.display.set_caption("LD30")
 screen_real = pygame.display.set_mode((WIDTH*SCALE, HEIGHT*SCALE), 0, 0)
@@ -37,10 +40,101 @@ class BaseCell:
 	solid = True
 
 	def draw(self, surf, x, y):
+		"""
+		Draws the cell to the surface "surf".
+		"""
 		pygame.draw.rect(surf, self.color, (x, y, 16, 16))
 
-	def is_solid(self, other):
+	def is_solid(self, world=None):
+		"""
+		Determines if this cell is solid or not.
+		"""
 		return self.solid
+
+class BaseEnt:
+	color = rgb(255, 0, 255)
+	rect = (4, 4, 8, 8)
+	world = 0
+
+	def __init__(self, lvl, cx, cy):
+		"""
+		Places an entity on level "lvl" at cell cx, cy.
+		"""
+		self.lvl = lvl
+		self.cx, self.cy = cx, cy
+		self.ox, self.oy =  0,  0
+
+	def draw(self, surf, x, y):
+		"""
+		Draws the entity at pixel position x, y.
+		"""
+		rx, ry, rw, rh = self.rect
+		rx += x + self.cx*16 + self.ox
+		ry += y + self.cy*16 + self.oy
+		pygame.draw.rect(surf, self.color, (rx, ry, rw, rh))
+
+	def tick(self):
+		"""
+		Logic update for entity.
+		"""
+
+		# To be overrided by subclasses.
+		pass
+
+class PlayerEnt(BaseEnt):
+	color = rgb(0, 255, 0)
+
+	# TODO: Move this function to a better spot eventually
+	def cell_is_walkable(self, cx, cy, world=None):
+		"""
+		Determine if we can walk onto a given cell.
+		"""
+
+		# Get the relevant "world" we're checking this for
+		if world == None:
+			world = self.world
+
+		# Get cell
+		cell = self.lvl.get_cell(cx, cy)
+
+		# Check if None
+		if cell == None:
+			return False # NEVER WALKABLE!
+
+		# Check if solid
+		return not cell.is_solid(world=world)
+
+	def tick(self):
+		# Move to relevant cell if need be
+		if self.ox != 0:
+			self.ox += (1 if self.ox < 0 else -1)
+		if self.oy != 0:
+			self.oy += (1 if self.oy < 0 else -1)
+
+		if self.ox == 0 and self.oy == 0:
+			# Work out movement
+			vx, vy = 0, 0
+			if newkeys[pygame.K_LEFT]:
+				vx -= 1
+			if newkeys[pygame.K_RIGHT]:
+				vx += 1
+			if newkeys[pygame.K_UP]:
+				vy -= 1
+			if newkeys[pygame.K_DOWN]:
+				vy += 1
+
+			# Work out if anything is in that cell
+			if self.cell_is_walkable(self.cx + vx, self.cy + vy):
+				self.cx += vx
+				self.cy += vy
+				self.ox -= 16*vx
+				self.oy -= 16*vy
+			elif self.cell_is_walkable(self.cx + vx, self.cy):
+				self.cx += vx
+				self.ox -= 16*vx
+			elif self.cell_is_walkable(self.cx, self.cy + vy):
+				self.cy += vy
+				self.oy -= 16*vy
 
 class FloorCell(BaseCell):
 	color = rgb(170, 100, 85)
@@ -55,6 +149,9 @@ class Level:
 	A Level contains all the logic and stuff for a, well, level.
 	"""
 	def __init__(self, data):
+		# Allocate entity list
+		self.ents = []
+
 		# Allocate grid
 		self.g = [[None for x in xrange(len(data[0]))]
 			for y in xrange(len(data))]
@@ -64,40 +161,57 @@ class Level:
 			for c, x in zip(l, xrange(len(l))):
 				self.g[y][x] = self.translate_level_char(c, x, y)
 
+	def get_cell(self, x, y):
+		"""
+		Gets a cell from the level for inspection or mutilation.
+		"""
+		if y < 0 or y >= len(self.g): return None
+		if x < 0 or x >= len(self.g[y]): return None
+		return self.g[y][x]
+
 	def tick(self):
-		# TODO.
+		"""
+		Logic update for level.
+		"""
+		# Update entities
+		for ent in self.ents:
+			ent.tick()
 		pass
 
 	def draw(self, surf, camx, camy):
 		"""
 		Draws the level at (-camx, -camy) on surface "surf".
 		"""
+
+		# Draw cells
 		for y in xrange(len(self.g)):
 			for x in xrange(len(self.g[0])):
 				cell = self.g[y][x]
 				if cell != None:
 					cell.draw(surf, x*16-camx, y*16-camy)
 
-		pygame.draw.rect(surf, rgb(0, 255, 0),
-			((self.player_x*16-camx)+4,
-			(self.player_y*16-camy)+4,
-			8, 8))
+		# Draw entities
+		for ent in self.ents:
+			ent.draw(surf, -camx, -camy)
 
 	def translate_level_char(self, c, x, y):
 		"""
-		Translates a character in the level source array
-		to... I need something better to write here
+		Maps a character from the source level to an actual class.
 		"""
+
 		if c == ".":
 			return None
+
 		elif c == ",":
 			return FloorCell()
+
 		elif c == "#":
 			return WallCell()
+
 		elif c == "P":
-			self.player_x = x
-			self.player_y = y
+			self.ents.append(PlayerEnt(self, x, y))
 			return FloorCell()
+
 		else:
 			raise Exception("invalid level char: %s" % repr(c))
 
@@ -110,10 +224,10 @@ lvl = Level(
 ..####,,,####...
 ..#P,#,,,#,,#...
 ..#,,,,,,,,,#...
-..#,,#####,,#...
-..#,,#...#,,#...
-..####...####...
-................
+..#,,,###,,,#...
+..#,,#,,,#,,#...
+..####,,,####...
+......###.......
 ................
 ................
 """.split("\n")[1:-1]
@@ -123,27 +237,35 @@ lvl = Level(
 quitflag = False
 oldkeys = pygame.key.get_pressed()
 newkeys = pygame.key.get_pressed()
+tick_next = time.time()
 while not quitflag:
-	# Draw screen
-	screen.fill(rgb(0,0,170))
-	lvl.draw(screen, 0, 0)
-	flip_screen()
+	# Handle timing
+	tick_current = time.time()
 
-	# Prevent CPU fires
-	# TODO: Proper frame timing
-	time.sleep(0.01)
+	if tick_current < tick_next:
+		# Draw screen
+		screen.fill(rgb(0,0,170))
+		lvl.draw(screen, 0, 0)
+		flip_screen()
 
-	# Poll events
-	pygame.event.pump()
-	newkeys = pygame.key.get_pressed()
-	if oldkeys[pygame.K_ESCAPE] and not newkeys[pygame.K_ESCAPE]:
-		quitflag = True
+		# Prevent CPU fires
+		time.sleep(0.01)
 
-	# Update logic
-	lvl.tick()
+	else:
+		# Poll events
+		pygame.event.pump()
+		newkeys = pygame.key.get_pressed()
+		if oldkeys[pygame.K_ESCAPE] and not newkeys[pygame.K_ESCAPE]:
+			quitflag = True
 
-	# Transfer newkeys to oldkeys
-	oldkeys = newkeys
+		# Update logic
+		lvl.tick()
+
+		# Transfer newkeys to oldkeys
+		oldkeys = newkeys
+
+		# Update tick counter
+		tick_next += SPF
 
 
 # Clean up
